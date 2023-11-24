7234472b78ad6e36062b7b49b471f9f3799d1484
dannysayres <138374245+dannysayres@users.noreply.github.com>
2023-11-24 06:07:09 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+document.querySelector(".button").addEventListener("click", refresh);
 
-
+function refresh(){   
 var randomNumber1 = Math.floor(Math.random()*6)+1;
 var randomDiceImage = "dice"+randomNumber1+".png";
 var randomImageSource = "images/"+randomDiceImage;
@@ -10,7 +11,6 @@ var randomDiceImage2 = "dice"+randomNumber2+".png";
 var randomImageSource2 = "images/"+randomDiceImage2;
 var image2 = document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
 
-    
 
 if(randomNumber1 > randomNumber2){
 document.querySelector("h1").innerText = "Kirsty Makes Tea";
@@ -20,3 +20,5 @@ document.querySelector("h1").innerText= "Danny Makes Tea";
 }else{
     document.querySelector("h1").innerText= "Its a draw";
 }
+}
+
